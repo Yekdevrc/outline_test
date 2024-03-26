30b6d5dcf7a53e1d445c2016f16ba08ca702fc2b
Yekdev Rokay <yekdevrc@gmail.com>
2024-03-26 16:49:30 +0545

    install breeze package for authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,54 +11,43 @@
     <title>Hello, world!</title>
   </head>
   <body>
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Welcome</a>
+          <a class="navbar-brand" href="#">Navbar</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ route('blog.index') }}">Blog</a>
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
           </div>
         </div>
       </nav>
 
-
-
-    <form action="{{ route('store') }}" method="POST">
-        @csrf
-    <div class="container mt-2">
-        <div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="name@example.com">
-        </div>
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" placeholder="name">
-        </div>
-            <input type="button" class="btn btn-sm btn-info" id="add" value="add" />
-        </div>
-    </div>
-    <table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">name</th>
-            <th scope="col">email</th>
-            <th scope="col">action</th>
-          </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-      </table>
-      <button type="submit" class="btn btn-primary btn-sm">Submit</button>
-    </form>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -69,38 +58,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <script>
-        $(document).ready(function(){
-            var i=0
-            $('#add').on('click', function(){
-                var details=[];
-                var name=$('#name').val()
-                var email=$('#email').val()
-                 ++i
-                details.push(name, email)
-                console.log(details)
-                var j=i
-                $('tbody').append(
-                    `<tr>
-                        <th scope="row">`+i+`</th>
-                        <td><input type="hidden" id="nameId" name="inputs[`+ j +`][name]" value=`+ details[0] +`>`+ details[0] +`</td>
-                        <td><input type="hidden" id="emailId" name="inputs[`+j+`][email]" value=`+ details[1] +`>`+ details[1] +`</td>
-                        <td><input type="button" class="btn btn-sm btn-danger" id="remove" value="remove" /></td>
-                      </tr>`
-                )
-                $('#name').val('')
-                $('#email').val('')
-                var nameid=$('#nameId').val()
-                var emailid=$('#emailId').val()
-                console.log(nameid, emailid)
-            })
-
-            $(document).on('click','#remove', function(){
-                $(this).parents('tr').remove()
-            })
-        })
-    </script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\BlogPostController;
-use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/store', [AdminController::class, 'store'])->name('store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('blog', BlogPostController::class);
-Route::resource('comments', CommentController::class)->except('index', 'edit', 'show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
